09e2f31597dc172cc570bc1ed84368c8c4c2453e
sameerkerketta@yahoo.co.in <sameerkerketta@yahoo.co.in>
2017-09-30 22:53:45 +0530

    300917

--- require/api.js
+++ require/api.js
@@ -3,8 +3,30 @@ var router = express.Router();
 var compact = require('underscore');
 var SofiaTree = require('sofia-tree');
 var fuzzy = require('fuzzy');
-var stopwords = ['year', 'live in', 'hq', 'hq version', 'hq video', 'best quality', 'hd colour', 'hd', 'original', 'full cd', 'with lyrics', 'w/ lyrics', 'in stereo', 'hq stereo', 'stereo', 'original version', 'original soundtrack', 'original', 'album version', 'lyrics', 'high quality audio', 'high quality', 'ost', 'promo version', 'promo', 'new stereo remix', 'new stereo', 'great audio quality ', 'great music video', 'audio quality', 'music video', 'official song', 'music quality', 'from mtv', 'lp version', 'official videoclip', 'original videoclip', 'official video', 'original video', 'rare video', 'rare version', 'version', 'itunes']
+var DecisionTree = require('decision-tree');
+var lev = require('fast-levenshtein');
+var stopwords = ['year', 'live in', 'hq', 'hq version', 'hq video', 'best quality', 'hd colour', 'hd', 'original', 'full cd', 'with lyrics', 'w/ lyrics', 'in stereo', 'hq stereo', 'stereo', 'original version', 'original soundtrack', 'original', 'album version', 'lyrics', 'high quality audio', 'high quality', 'ost', 'promo version', 'promo', 'new stereo remix', 'new stereo', 'great audio quality ', 'great music video', 'audio quality', 'music video', 'official song', 'music quality', 'from mtv', 'lp version', 'official videoclip', 'original videoclip', 'official video', 'original video', 'rare video', 'rare version', 'version', 'itunes', 'official music video']
 var typsng = ['ain\'t', 'alone', 'angel', 'arms', 'around', 'away', 'baby', 'bad', 'beautiful', 'believe', 'blue', 'boy', 'change', 'christmas', 'comes', 'crazy', 'cry', 'dance', 'days', 'dear', 'dream', 'ever', 'everybody', 'everything', 'eyes', 'fall', 'feel', 'fire', 'fool', 'forever', 'girl', 'gone', 'gonna', 'goodbye', 'happy', 'heart', 'heaven', 'hey', 'hold', 'kiss', 'la', 'lady', 'leave', 'life', 'light', 'lonely', 'love', 'lover', 'mama', 'man', 'mind', 'mine', 'miss', 'moon', 'moonlight', 'morning', 'mr', 'music', 'night', 'nobody', 'oh', 'people', 'play', 'please', 'rain', 'red', 'remember', 'river', 'rock', 'roll', 'rose', 'sing', 'smile', 'somebody', 'something', 'song', 'soul', 'star', 'stay', 'stop', 'street', 'summer', 'sun', 'sweet', 'sweetheart', 'talk', 'tears', 'theme', 'things', 'think', 'tonight', 'town', 'true', 'walk', 'wanna', 'wish', 'woman', 'wonderful', 'world', 'young']
+
+class DtClass {
+    constructor(firstposition, lastposition, status) {
+        this.firstposition = firstposition;
+        this.lastposition = lastposition;
+        this.status = status;
+    }
+
+    getFirstPosition() {
+        return this.firstposition;
+    }
+
+    getLastPosition() {
+        return this.lastposition;
+    }
+
+    getStatus() {
+        return this.status;
+    }
+}
 
 router.get('/qrytitle', function (req, res) {
     var m = require('../server').initnamedb();
@@ -24,18 +46,17 @@ router.get('/qrytitle', function (req, res) {
         song = song.toLowerCase().replace(/\&/g, "and").replace(/\" by/g, " \-").replace(/\"/g, "");
 
         //mtcharr = song.match(new RegExp(/[\w\'\!\s]+(?:[- ]\w+)*/g));
+
+
+
+        //var dd = compact.difference(cnvr2arr(song), stopwords);
+
+
+        song = retsng(song, stopwords)
+
+        song = song.replace(/[\(\{\[]\s*[\]\}\)]/g, "");
+
         mtcharr = song.split(/-|~/g);
-
-
-        var dd = compact.difference(cnvr2arr(song), stopwords);
-
-        mtcharr.forEach(function (mtchstr, i) {
-            stopwords.forEach(function (stopstr) {
-                if (~mtchstr.indexOf(stopwords)) {
-                    mtcharr[i] = "";
-                }
-            });
-        });
 
         compact.compact(mtcharr);
         var retsngr = [];
@@ -52,22 +73,65 @@ router.get('/qrytitle', function (req, res) {
                     });
                     var a = findLongestCommonSubstring_Quick(song, fndslt.join(" "));
                     var retval = fndslt.indexOf(a.trim())
+
+
+
                     if (retval != undefined && retval > -1) {
-                        var snglst = a.trim().split(/,|and/g)
-                        if (snglst > 0) {
-                            sngrlst.forEach(function (sngrvar) {
-                                retsngr.push(sngrvar);
-                            });
-                        } else
-                            retsngr.push(a.trim());
+                        var td = []
+                        for (l = 1; l < song.length; l++) {
+                            for (t = l; t <= song.length; t++) {
+                                if (l === 1)
+                                    td.push(new DtClass(l, t, "ok"));
+                                else if ((l > 1) && (t < song.length))
+                                    td.push(new DtClass(l, t, "nok"));
+                                else if ((l > 1) && (t === song.length))
+                                    td.push(new DtClass(l, t, "ok"));
+                            }
+                        }
+
+                        var features = ["firstposition", "lastposition"];
+                        var class_name = "status";
+                        dt = new DecisionTree(td, class_name, features);
+
+                        var predicted_class = dt.predict({
+                            firstposition: song.indexOf(a.trim()) + 1,
+                            lastposition: song.indexOf(a.trim()) + a.trim().length
+                        });
+
+                        if (predicted_class === "ok") {
+                            var snglst = a.trim().split(/,|and/g)
+                            if (snglst > 0) {
+                                sngrlst.forEach(function (sngrvar) {
+                                    retsngr.push(sngrvar);
+                                });
+                            } else
+                                retsngr.push(a.trim());
+                        }
                     }
                 }
             });
             retsngr = compact.uniq(retsngr);
-            res.send('{"song":"' + retsng(song, retsngr) + '","singer":' + JSON.stringify(retsngr) + '}');
+            if (retsngr.length === 2) {
+                var levdst = lev.get(retsngr[0] + " and " + retsngr[1], song.substring(song.indexOf(retsngr[0]), song.indexOf(retsngr[1]) + retsngr[1].length));
+                if (levdst < 5)
+                    res.send('{"song":"' + retsng(song, retsngr) + '","singer":' + JSON.stringify(retsngr) + '}');
+                else {
+                    var tmp = [];
+                    if (countWords(retsngr[0]) > countWords(retsngr[1])) {
+                        tmp.push(retsngr[0]);
+                        res.send('{"song":"' + retsng(song, tmp) + '","singer":' + JSON.stringify(tmp) + '}');
+                    } else {
+                        tmp.push(retsngr[1]);
+                        res.send('{"song":"' + retsng(song, tmp) + '","singer":' + JSON.stringify(tmp) + '}');
+                    }
+                }
+            } else if (retsngr.length === 1) {
+                res.send('{"song":"' + retsng(song, retsngr) + '","singer":' + JSON.stringify(retsngr) + '}');
+            }
         } else {
             for (k = 0; k < mtcharr.length; k++) {
                 var sngarr = cnvr2arr(mtcharr[k]);
+
                 sngarr.forEach(function (m, idx) {
                     fndslt = sofiaTree.getCompletions(m);
                     if (fndslt.length > 0) {
@@ -77,13 +141,15 @@ router.get('/qrytitle', function (req, res) {
                         var a = findLongestCommonSubstring_Quick(song, fndslt.join(" "));
                         var retval = fndslt.indexOf(a.trim())
                         if (retval != undefined && retval > -1) {
-                            var snglst = a.trim().split(/,|and/g)
-                            if (snglst.length > 0) {
-                                snglst.forEach(function (sngrvar) {
-                                    retsngr.push(sngrvar.trim());
-                                });
-                            } else
-                                retsngr.push(a.trim());
+                            if (occpspc(a.trim(), sngarr.length) === true) {
+                                var snglst = a.trim().split(/,|and/g)
+                                if (snglst.length > 0) {
+                                    snglst.forEach(function (sngrvar) {
+                                        retsngr.push(sngrvar.trim());
+                                    });
+                                } else
+                                    retsngr.push(a.trim());
+                            }
                         }
                         if (retsngr.length > 0 && retsong === "") {
                             if (k === 0)
@@ -96,13 +162,26 @@ router.get('/qrytitle', function (req, res) {
                 });
             }
             retsngr = compact.uniq(retsngr);
+
             res.send('{"song":"' + retsong + '","singer":' + JSON.stringify(retsngr) + '}');
+
+
         }
     }, 3000)
 
 });
 
 module.exports = router;
+
+function occpspc(m, lenofelem) {
+    var t = cnvr2arr(m);
+    var k = t.length / lenofelem * 100;
+    if (k >= 40)
+        return true;
+    else
+        return false;
+}
+
 
 
 function cnvr2arr(tmp) {
@@ -143,6 +222,9 @@ findLongestCommonSubstring_Quick = function (a, b) {
     return longest;
 }
 
+function countWords(str) {
+    return str.trim().split(/\s+/).length;
+}
 
 function retsng(song, cmprarr) {
     cmprarr.forEach(function (b) {
